31ec749d0e8c1ef0916b6010109d5f61647636c8
Guy Assa <78320514+theAguy@users.noreply.github.com>
2024-07-24 22:42:57 +0300

    Add files via upload

---
+++ crispector2/algorithm/binomial_probability.py
@@ -0,0 +1,120 @@
+from crispector2.utils.constants_and_types import Pr, IndelType, C_TX, C_MOCK, ON_TARGET, CUT_SITE, SITE_NAME, R_PRIMER, F_PRIMER, \
+    BINOM_PERCENTILE, BINOM_AVERAGE_P, AmpliconDf
+from crispector2.utils.logger import LoggerWrapper
+from crispector2.utils.configurator import Configurator
+from typing import Dict, List
+from crispector2.modifications.modification_tables import ModificationTables
+from crispector2.modifications.modification_types import ModificationTypes
+import numpy as np
+
+
+def compute_binom_p(tables: Dict[str, ModificationTables], modifications: ModificationTypes,
+                    override_coin: bool, ref_df: AmpliconDf) -> Dict[str, List[Pr]]:
+    """
+    Compute binomial probability for each site.
+    :param tables: Modification tables
+    :param modifications: Modification Types
+    :param override_coin:
+    :param ref_df:
+    :return: Dict[SITE_NAME, LIST of probability for each modification type]
+    """
+
+    logger = LoggerWrapper.get_logger()
+    cfg = Configurator.get_cfg()
+    binom_p_d = dict()
+    default_q = cfg["NHEJ_inference"]["default_q"]  # the probability of an indel to occur through an edit event
+    default_q_warning_text = "Will use default q parameter (= {}) from config file for the Bayesian NHEJ inference " \
+                             "(check CRISPECTOR paper for more details).".format(default_q)
+    # Use default Binomial probability when can't estimate signal from on target.
+    n_on_target_site = 0
+    for site_name in list(ref_df[ref_df[ON_TARGET] == True][SITE_NAME]):
+        if '[' not in site_name:
+            n_on_target_site += 1
+    if n_on_target_site > 1:
+        override_coin = True
+        logger.warning("Experiment has Multiple on-target sites. {}".format(default_q_warning_text))
+    elif n_on_target_site == 0:
+        override_coin = True
+        logger.warning("Experiment doesn't have on-target site. {}".format(default_q_warning_text))
+    else:
+        on_site_name = ref_df.loc[ref_df[ON_TARGET], SITE_NAME].values[0]
+        if on_site_name not in tables:
+            override_coin = True
+            logger.warning("On-target site was discarded from evaluation. {}".format(default_q_warning_text))
+        elif ref_df.shape[0] == 1:
+            override_coin = True
+            logger.warning("Experiment doesn't have off-target sites. {}".format(default_q_warning_text))
+
+    # Use default_p if override_coin is True
+    if override_coin:
+        for site_name in tables.keys():
+            binom_p_d[site_name] = modifications.size*[default_q]
+        return binom_p_d
+
+    n_on = tables[on_site_name].n_reads_tx
+    p_d = dict()  # probability dict, for all indels and sites
+    for indel_type in [IndelType.DEL, IndelType.INS]:
+        e_on = 0
+        site_noise_d = dict() # Keys - sites, values - mock noise vector (for each position)
+        p_d[indel_type] = dict()
+
+        # Aggregate all tables for current indel_type
+        for table_idx in range(modifications.size):
+            if indel_type != modifications.types[table_idx]:
+                continue
+
+            # Concatenate alpha from all sites
+            for site_name, site_table in tables.items():
+                table = site_table.tables[table_idx]
+
+                # Calculate e_on - number of modifications ("edits") on the cut-site
+                if site_name == on_site_name:
+                    cut_site = ref_df.loc[site_name, CUT_SITE]
+                    # For insertion - edit on the cut-site
+                    if indel_type == IndelType.INS:
+                        e_on += site_table.tables[table_idx][C_TX, cut_site]
+                    # For deletion - max between left and right to the cut-site
+                    else:
+                        e_on += np.max(site_table.tables[table_idx][C_TX, cut_site:cut_site + 1])
+
+                    # Don't compute alpha from on-target due to possible contamination
+                    continue
+
+                # Compute alpha - percentage of "noise" on each position outside the primers
+                f_primer = len(ref_df.loc[site_name, F_PRIMER])
+                r_primer = len(ref_df.loc[site_name, R_PRIMER])
+                if site_name in site_noise_d:
+                    site_noise_d[site_name] += table[C_MOCK, f_primer:-r_primer] / site_table.n_reads_mock
+                else:
+                    site_noise_d[site_name] = table[C_MOCK, f_primer:-r_primer] / site_table.n_reads_mock
+
+        # Estimate experiment noise vector from all Concatenated site noise values
+        experiment_noise = np.zeros(len(site_noise_d))
+        for site_idx, site_noise in enumerate(site_noise_d.values()):
+            site_noise = site_noise[site_noise > 0]
+            if len(site_noise) != 0:
+                # Percentile on different position in the same site
+                experiment_noise[site_idx] = np.percentile(site_noise, BINOM_PERCENTILE)
+
+        # Estimate Binomial probability for each site
+        for site_name, site_table in tables.items():
+            alpha = np.percentile(experiment_noise, BINOM_PERCENTILE)  # Percentile on different sites
+            n_mock = site_table.n_reads_mock
+            n_tx = site_table.n_reads_tx
+            binom_p = e_on / (e_on + (alpha * n_on * n_mock / n_tx))
+            p_d[indel_type][site_name] = (binom_p + BINOM_AVERAGE_P) / 2 # Average for stability
+
+    # Convert p_d to crispector algorithm format
+    for site_name in tables.keys():
+        binom_p_d[site_name] = []
+        for indel_type in modifications.types:
+            if indel_type in [IndelType.DEL, IndelType.INS]:
+                binom_p_d[site_name].append(p_d[indel_type][site_name])
+            # For mixed indels - average insertions and deletions
+            elif indel_type == IndelType.MIXED:
+                binom_p_d[site_name].append((p_d[IndelType.DEL][site_name] + p_d[IndelType.INS][site_name]) / 2)
+            # For substitutions - use the default coin
+            elif indel_type == IndelType.SUB:
+                binom_p_d[site_name].append(default_q)
+
+    return binom_p_d
---
+++ crispector2/algorithm/translocations.py
@@ -0,0 +1,134 @@
+from collections import defaultdict
+import numpy as np
+from math import exp
+from numpy import exp, log1p
+# from scipy.special import logsumexp
+from scipy import stats
+from statsmodels.stats.multitest import fdrcorrection
+import pandas as pd
+from crispector2.utils.constants_and_types import TransDf, AlgResultDf, EDIT_PERCENT, TransResultDf, SITE_NAME, \
+    R_SITE, L_SITE, FREQ, TX_READ_NUM, MOCK_READ_NUM, SITE_A, SITE_B, TX_TRANS_READ, MOCK_TRANS_READ, TRANS_PVAL, \
+    TRANS_FDR, IS_TRANS, TX_TRANS_BACKGROUND_READ, MOCK_TRANS_BACKGROUND_READ
+
+import mpmath
+from mpmath import exp
+
+def translocations_test(result_df: AlgResultDf, tx_df: TransDf, mock_df: TransDf, FDR_thresh: float,
+                        edit_thresh: float) -> TransResultDf:
+    """
+
+    :param result_df: NHEJ activity results
+    :param tx_df: Treatment translocations
+    :param mock_df: Mock translocations
+    :param FDR_thresh: Threshold to count FDR value as active
+    :param edit_thresh: Threshold to count NHEJ activity as active
+    :return: TransResultDf - translocation result
+    """
+    if tx_df.shape[0] == 0:
+        return pd.DataFrame(columns=[SITE_A, SITE_B, TX_TRANS_READ, MOCK_TRANS_READ, TRANS_PVAL, TRANS_FDR])
+
+    trans_d = defaultdict(list)
+
+    # Check translocation only for active editing site.
+    active_df_t = result_df.loc[result_df[EDIT_PERCENT] >= edit_thresh]
+    active_df = active_df_t.reset_index(drop=True)
+    # Run HG test for all translocations
+    for row_idx, row_a in active_df.iterrows():
+        for _, row_b in active_df.iloc[row_idx+1:].iterrows():
+            site_a = row_a[SITE_NAME]
+            site_b = row_b[SITE_NAME]
+
+            # Get all translocation between site A & B
+            tx_a_b_df = tx_df.loc[((tx_df[R_SITE] == site_a) & (tx_df[L_SITE] == site_b)) |
+                                  ((tx_df[R_SITE] == site_b) & (tx_df[L_SITE] == site_a))]
+            tx_trans = tx_a_b_df[FREQ].sum()
+
+            # Make sure mock_df is empty with all the relevant fields
+            if (R_SITE in mock_df.columns) and (L_SITE in mock_df.columns):
+                mock_a_b_df = mock_df.loc[((mock_df[R_SITE] == site_a) & (mock_df[L_SITE] == site_b)) |
+                                          ((mock_df[R_SITE] == site_b) & (mock_df[L_SITE] == site_a))]
+                mock_trans = mock_a_b_df[FREQ].sum()
+            else:
+                mock_trans = 0
+
+            # Get number for HG test
+            tx_a_read_num = row_a[TX_READ_NUM]
+            mock_a_read_num = row_a[MOCK_READ_NUM]
+            tx_b_read_num = row_b[TX_READ_NUM]
+            mock_b_read_num = row_b[MOCK_READ_NUM]
+
+            # Tx & Mock background number are Geometric average of site A & B
+            tx_read_num = (tx_a_read_num * tx_b_read_num) ** 0.5
+            mock_read_num = (mock_a_read_num * mock_b_read_num) ** 0.5
+
+            # Compute p-value
+            b = tx_trans
+            B = tx_trans + mock_trans
+            n = tx_read_num
+            N = tx_read_num + mock_read_num
+
+            # If both sites have no translocations than move to the next couple
+            if B == 0:
+                continue
+
+            #p_val = hypergeometric_cdf(b - 1, N, B, n)
+            p_val = hypergeometric_cdf_log(b - 1, N, B, n) #changed tp mpmath implementation
+            # Store values
+            trans_d[SITE_A].append(site_a)
+            trans_d[SITE_B].append(site_b)
+            trans_d[TX_TRANS_READ].append(tx_trans)
+            trans_d[MOCK_TRANS_READ].append(mock_trans)
+            trans_d[TX_TRANS_BACKGROUND_READ].append(int(tx_read_num))
+            trans_d[MOCK_TRANS_BACKGROUND_READ].append(int(mock_read_num))
+            trans_d[TRANS_PVAL].append(p_val)
+
+    # Compute FDR correction
+    _, trans_d[TRANS_FDR] = fdrcorrection(trans_d[TRANS_PVAL])
+    if len(trans_d) != 0:
+        trans_df: TransResultDf = pd.DataFrame.from_dict(trans_d)
+    else:
+        trans_df = pd.DataFrame(columns=[SITE_A, SITE_B, TX_TRANS_READ, MOCK_TRANS_READ, TRANS_PVAL, TRANS_FDR])
+
+    trans_df = trans_df.sort_values(by=[TRANS_FDR]).reset_index(drop=True)
+
+    # Set Is_translocation column in tx_read and mock_read
+    mock_df[IS_TRANS] = False
+    tx_df[IS_TRANS] = False
+    active_trans_df = trans_df.loc[trans_df[TRANS_FDR] < FDR_thresh]
+
+    for _, row in active_trans_df.iterrows():
+        site_a = row[SITE_A]
+        site_b = row[SITE_B]
+        tx_df.loc[((tx_df[R_SITE] == site_a) & (tx_df[L_SITE] == site_b)) |
+                  ((tx_df[R_SITE] == site_b) & (tx_df[L_SITE] == site_a)), IS_TRANS] = True
+
+    return trans_df
+
+
+def hypergeom_pmf(k, M, n, N):
+    '''
+    implementing the probability mass function using mpmath package
+    '''
+
+    # k = number of translocations in Tx sample
+    # M = total reads (Mock+Tx)
+    # n = Total number of translocations (Tx + M)
+    # N = total number of Tx reads
+
+    mock_n = M - n  # number of mock reads
+    pmf = (mpmath.beta(n + 1, 1) * mpmath.beta(mock_n + 1, 1) * mpmath.beta(M - N + 1, N + 1) /
+           (mpmath.beta(k + 1, n - k + 1) * mpmath.beta(N - k + 1, mock_n - N + k + 1) * mpmath.beta(M + 1, 1)))
+
+    return pmf
+
+
+def hypergeometric_cdf_log(k, M, n, N):
+    """
+      log pmf calculation using mpmath
+    """
+    max_val = min(N + 1, n + 1)  # hypergemotric test upper limit
+    k2 = np.arange(k + 1, max_val)
+    all_pmfs = [mpmath.log(hypergeom_pmf(k_ind, M, n, N)) for k_ind in k2]  # calclulate the logpmf
+
+    log_res = mpmath.log(mpmath.fsum(mpmath.exp(val) for val in all_pmfs))  # log sum all distribution values
+    return exp(log_res)
