e56eba002bc3ab08f5e4087d242be9bdb4f6a1d9
Guy Assa <78320514+theAguy@users.noreply.github.com>
2024-07-24 22:46:42 +0300

    Add files via upload

---
+++ crispector2/modifications/modification_tables.py
@@ -0,0 +1,174 @@
+from crispector2.input_processing.utils import parse_cigar_with_mixed_indels
+from crispector2.utils.constants_and_types import ReadsDf, DNASeq, IndelType, ExpType, ModTables, ModTablesP, IsEdit, CIGAR, FREQ, \
+    C_TX, C_MOCK, REFERENCE, CS_SHIFT_R, CS_SHIFT_L, ModDist, MOD_TABLE_IDX, INDEL_TYPE, INDEL_LEN, IS_EDIT, \
+    POS_IDX_S, POS_IDX_E
+from crispector2.modifications.modification_types import ModificationTypes
+import numpy as np
+from collections import defaultdict
+import pandas as pd
+from copy import deepcopy
+
+
+class ModificationTables:
+    """
+    A container class for all modification (Indel) tables.
+    """
+    def __init__(self, tx_reads: ReadsDf, mock_reads: ReadsDf, modifications: ModificationTypes, ref_df_row: pd.Series):
+        self._tx_reads = tx_reads
+        self._mock_reads = mock_reads
+        self._modifications = modifications
+        self._amplicon = ref_df_row[REFERENCE]
+        self._tables: ModTables = dict()
+        self._pointers: ModTablesP = dict()
+        self._tx_dist: ModDist = pd.DataFrame()  # Tx modification distribution - for visualization only
+        self._mock_dist: ModDist = pd.DataFrame()  # Mock modification distribution - for visualization only
+        self._create_modification_tables_and_distribution()
+        self._n_reads_tx = self._tx_reads[FREQ].sum()
+        self._n_reads_mock = self._mock_reads[FREQ].sum()
+        self._priors = deepcopy(self._modifications.priors)
+
+    # Getters
+    @property
+    def priors(self):
+        return self._priors
+
+    @property
+    def tx_reads(self) -> ReadsDf:
+        return self._tx_reads
+
+    @property
+    def mock_reads(self) -> ReadsDf:
+        return self._mock_reads
+
+    @property
+    def tx_dist(self) -> ReadsDf:
+        return self._tx_dist
+
+    @property
+    def mock_dist(self) -> ReadsDf:
+        return self._mock_dist
+
+    @property
+    def n_reads_tx(self) -> int:
+        return self._n_reads_tx
+
+    @property
+    def n_reads_mock(self) -> int:
+        return self._n_reads_mock
+
+    @property
+    def tables(self) -> ModTables:
+        return self._tables
+
+    @property
+    def pointers(self) -> ModTablesP:
+        return self._pointers
+
+    @property
+    def amplicon(self) -> DNASeq:
+        return self._amplicon
+
+    # setters
+    @tx_reads.setter
+    def tx_reads(self, value):
+        self._tx_reads = value
+
+    @mock_reads.setter
+    def mock_reads(self, value):
+        self._mock_reads = value
+
+    @n_reads_tx.setter
+    def n_reads_tx(self, value):
+        self._n_reads_tx = value
+
+    @n_reads_mock.setter
+    def n_reads_mock(self, value):
+        self._n_reads_mock = value
+
+    def _create_modification_tables_and_distribution(self):
+        """
+        Function create modification tables and pointers to ReadsDf for all modification types.
+        Also - function creates modification distribution data.
+        :return: mock table list and mock pointers
+        """
+        # Create table and pointers
+        for idx, indel_type in enumerate(self._modifications.types):
+            # Insertions are between positions (so they have an extra item)
+            table_size = len(self._amplicon) + 1 if indel_type == IndelType.INS else len(self._amplicon)
+            self._tables[idx] = np.zeros((2, table_size), dtype=np.int64)
+            self._pointers[idx] = defaultdict(list)
+
+        # Fill the tables
+        self._tx_dist = self._convert_read_df_to_modifications(self._tx_reads, ExpType.TX)
+        self._mock_dist = self._convert_read_df_to_modifications(self._mock_reads, ExpType.MOCK)
+
+    def _convert_read_df_to_modifications(self, read: ReadsDf, exp_type: ExpType) -> ModDist:
+        """
+        Fill (inplace!) the modification tables from all reads.
+        For treatment - fill inplace pointers as well.
+        For modification distribution - return the modification distribution
+        This function could be split to two different function. The only reason to combine the logic is to reduce
+        running time. runtime is O(num_of_reads*num_of_modification)
+        :param read: ReadsDf
+        :param exp_type: ExpType
+        :return: ModDist
+        """
+        table_row = C_TX if exp_type == ExpType.TX else C_MOCK
+        dist_d = defaultdict(list)
+
+        for row_idx, row in read.iterrows():
+            pos_idx = 0  # position index
+            for length, length_wo_ins, indel_type, mixed_l in parse_cigar_with_mixed_indels(row[CIGAR]):
+                table_idx = self._modifications.find_index(indel_type, length)
+                # For a match - continue
+                if indel_type == IndelType.MATCH:
+                    pos_idx += length_wo_ins
+                    continue
+                # For a mismatch or deletions - update multi indexes according to length
+                elif indel_type in [IndelType.DEL, IndelType.SUB, IndelType.MIXED]:
+                    self._tables[table_idx][table_row, pos_idx:pos_idx+length_wo_ins] += row[FREQ]
+                    # Update multiple keys
+                    if exp_type == ExpType.TX:
+                        for pointer_idx in range(pos_idx, pos_idx+length_wo_ins):
+                            self._pointers[table_idx][pointer_idx].append(row_idx)
+                # For an insertion update single index and don't increase pos_idx
+                elif indel_type == IndelType.INS:
+                    self._tables[table_idx][table_row, pos_idx] += row[FREQ]
+                    if exp_type == ExpType.TX:
+                        self._pointers[table_idx][pos_idx].append(row_idx)
+
+                # Add modification to distribution
+                indel_l = [(length, indel_type)] if indel_type != IndelType.MIXED else mixed_l
+                for dist_length, dist_indel_type in indel_l:
+                    dist_d[MOD_TABLE_IDX].append(table_idx)
+                    dist_d[INDEL_TYPE].append(dist_indel_type)
+                    dist_d[INDEL_LEN].append(dist_length)
+                    dist_d[FREQ].append(row[FREQ])
+                    dist_d[IS_EDIT].append(False)
+                    dist_d[POS_IDX_S].append(pos_idx)
+                    end_idx = pos_idx + length_wo_ins if indel_type != IndelType.INS else pos_idx + 1
+                    dist_d[POS_IDX_E].append(end_idx)
+
+                # Increase index
+                pos_idx += length_wo_ins
+
+        # Convert dict to DataFrame
+        dist_df = pd.DataFrame.from_dict(dist_d, orient='columns')
+        return dist_df
+
+    def set_tx_dist_is_edit_col(self, edit: IsEdit, table_offset: int, window_size: int):
+        """
+        Mark all edited modifications from table and algorithm output (edit)
+        :param edit: IsEdit
+        :param table_offset:
+        :param window_size:
+        :return:
+        """
+        edit_size = {IndelType.INS: 2*window_size, IndelType.SUB: 2*window_size-1, IndelType.DEL: 2*window_size-1}
+        for row_idx, row in self._tx_dist.iterrows():
+            # modification start and end truncated by qualification window
+            mod_s = max(row[POS_IDX_S], table_offset)
+            mod_e = min(row[POS_IDX_E], table_offset + edit_size[row[INDEL_TYPE]])
+            mod_range = range(mod_s - table_offset, mod_e - table_offset)
+            if edit[row[MOD_TABLE_IDX]][mod_range].any():
+                self._tx_dist.at[row_idx, IS_EDIT] = True
---
+++ crispector2/modifications/modification_types.py
@@ -0,0 +1,124 @@
+from crispector2.utils.exceptions import PriorPositionHasWrongLength
+from crispector2.utils.constants_and_types import IndelType
+from typing import List
+from collections import defaultdict
+from crispector2.utils.configurator import Configurator
+
+
+
+class ModificationTypes:
+    """
+    A container class for all modification (Indel) types
+    """
+    def __init__(self, indel_type: List[IndelType], min_l: List[int], max_l: List[int], priors: List[List[float]],
+                 max_indel_size):
+        """
+        :param indel_type: IndelType
+        :param min_l: indel_type length min
+        :param max_l: indel_type length min
+        :param priors: prior for each modification type
+        :param max_indel_size: max number of indel (from config file)
+        """
+        self._type = indel_type
+        self._range = []
+        for indel_min, indel_max in zip(min_l, max_l):
+            self._range.append(range(indel_min, indel_max+1))
+        self._priors = priors
+        self._max_indel_size = max_indel_size
+        self._type_d = defaultdict(list)
+        for idx, (indel_type, indel_range) in enumerate(zip(self._type, self._range)):
+            self._type_d[indel_type].append((indel_range, idx))
+
+    @property
+    def types(self):
+        return self._type
+
+    @property
+    def ranges(self):
+        return self._range
+
+    @property
+    def priors(self):
+        return self._priors
+
+    @property
+    def size(self):
+        return len(self._type)
+
+    def find_index(self, indel_type: IndelType, length: int) -> int:
+        """
+        Function returns the index of the modification type
+        :param indel_type:
+        :param length:
+        :return: modification type index
+        """
+        if indel_type == IndelType.MATCH:
+            return -1
+
+        index = 0
+        for indel_range, indel_idx in self._type_d[indel_type]:
+            if length in indel_range:
+                index = indel_idx
+                break
+        return index
+
+    @classmethod
+    def init_from_cfg(cls, enable_substitutions: bool):
+        """
+        Parse the config file to ModificationTypes class
+        :param enable_substitutions: Flag
+        :return: ModificationTypes.
+        """
+        cfg = Configurator.get_cfg()["NHEJ_inference"]
+        indel_types_cfg = cfg["IndelTypes"]
+        window_size = cfg["window_size"]
+        m_list = []
+        m_min = []
+        m_max = []
+        m_prior = []
+        for indel_type in IndelType:
+            if indel_type == IndelType.MATCH:
+                continue
+            for key, indel_dict in indel_types_cfg[indel_type.name].items():
+                m_list.append(indel_type)
+                m_min.append(indel_dict["min"])
+                m_max.append(indel_dict["max"])
+
+                # Extract priors
+                pos_prior = indel_dict["pos_prior"]
+                # If enable_substitutions is false, change priors to zero
+                if not enable_substitutions and (indel_type == IndelType.SUB):
+                    pos_prior = len(pos_prior)*[0.0]
+
+                prior_expected_len = (2*window_size + 1) if indel_type == IndelType.INS else 2*window_size
+                if len(pos_prior) != prior_expected_len:
+                    raise PriorPositionHasWrongLength(prior_expected_len, len(pos_prior), indel_type, m_min[-1],
+                                                      m_max[-1])
+                m_prior.append(pos_prior)
+
+        return cls(m_list, m_min, m_max, m_prior, cfg["max_indel_size"])
+
+    def name_at_idx(self, idx: int) -> str:
+        """
+        Return modification name by index
+        :param idx:
+        :return:
+        """
+        max_range = "INF" if self._max_indel_size == max(self._range[idx]) else max(self._range[idx])
+        return "{} ({}-{})".format(self._type[idx].name, min(self._range[idx]), max_range)
+
+    def plot_name_at_idx(self, idx: int) -> str:
+        """
+        Return modification plot name (identical size) by index
+        :param idx:
+        :return:
+        """
+        if self._max_indel_size == max(self._range[idx]):
+            return r"{} of len $\geq {}$".format(self._type[idx].plot_name, min(self._range[idx]))
+        elif min(self._range[idx]) == max(self._range[idx]):
+            return r"{} of len ${}$".format(self._type[idx].plot_name, min(self._range[idx]))
+        else:
+            return r"{} of len ${}:{}$".format(self._type[idx].plot_name, min(self._range[idx]), max(self._range[idx]))
+
+
+
